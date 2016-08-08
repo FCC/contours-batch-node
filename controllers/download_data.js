6e7bcf3bcfad0635f112b347ae1a0c2e85b39913
qihangli <qihang.li@yahoo.com>
2016-08-08 16:20:18 -0400

    batch fetch

--- controllers/download_data.js
+++ controllers/download_data.js
@@ -16,7 +16,8 @@ var access_key = configEnv[NODE_ENV].ACCESS_KEY;
 var secret_key = configEnv[NODE_ENV].SECRET_KEY;
 
 var request = require('request');
-var fs = require('fs');
+var fs = require('fs-extra');
+var unzip = require('unzip');
 var AWS = require('aws-sdk');
 
 AWS.config.update({
@@ -32,20 +33,43 @@ AWS.config.update({
 var s3 = new AWS.S3();
 
 
+//fetched files
+var dum = fs.readFileSync(data_dir + '/file_list.txt', 'utf-8');
+var files = dum.split('\n')
+var fetched_files = [];
+var dum1;
+
+for (var i = 0; i < files.length; i++) {
+	dum = files[i].split(' ');
+	if (dum[0] != '') {
+		fetched_files.push(dum[0]);
+	}
+}
+
 var lat, lon, ns, ew, filename;
 var root_url = 'https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/1/GridFloat';
 
-for (lat = 40; lat >= 38; lat--) {
-	for (lon = -100; lon <= -98; lon++) {
+var filename = [];
+var filepath = [];
+var url = [];
 
-	filename = makeFileName(lat, lon);
-	//console.log(filename);
-	var url = root_url + '/' + filename;
-	var filepath = data_dir + '/' + filename;
-	getFile(url, filename, filepath);
-	
+
+for (lat = 40; lat >= 39; lat--) {
+	for (lon = -117; lon <= -99; lon++) {
+		var fi = makeFileName(lat, lon);
+		var fi_flt = fi.replace('.zip', '.flt').toLowerCase();
+		console.log(fi_flt);
+		
+		if (fetched_files.indexOf(fi_flt) < 0) {
+			filename.push(fi);
+			url.push(root_url + '/' + fi);
+			filepath.push(data_dir + '/dummy/' + fi);
+		}
 	}
 }
+
+
+getFile(0);
 
 
 function makeFileName(lat, lon) {
@@ -80,11 +104,16 @@ function padZero(a, n) {
 	return a_str;
 }
 
-function getFile(url, filename, filepath) {
-	//console.log('filepath: ' + filepath);
-	//console.log('start download:')
-	//console.log(new Date());
-	request({url: url, encoding: null, rejectUnauthorized: false, strictSSL: false}, function (err, response, body) {
+function getFile(n) {
+	console.log('n: ' + n);
+	console.log('filepath: ' + filepath[n]);
+	console.log('url: ' + url[n]);
+	console.log('start download:')
+	console.log(new Date());
+	
+	fs.emptyDirSync(data_dir + '/dummy');
+	
+	request({url: url[n], encoding: null, rejectUnauthorized: false, strictSSL: false}, function (err, response, body) {
 		
 		if (err) {
 				
@@ -100,8 +129,11 @@ function getFile(url, filename, filepath) {
 			};  
 				
 			res.status(500);
-			//res.send(err_res);				
-			
+			//res.send(err_res);
+			if (n+1 < url.length) {
+				getFile(n+1);
+			}			
+
 		}
 		else {
 		
@@ -109,43 +141,60 @@ function getFile(url, filename, filepath) {
 			//console.log('response.headers[content-type] : ' + response.headers['content-type']);
 			//console.log('response.headers : ' + JSON.stringify(response.headers) );
 			
-			console.log('filename: ' + filename + ' status: ' + response.statusCode);
+			console.log('filename: ' + filename[n] + ' status: ' + response.statusCode);
+			var flt_filename = filename[n].replace('.zip', '.flt').toLowerCase();
+			var flt_filepath = data_dir + '/dummy/' + flt_filename;
 			if (response.statusCode == 200) {
 			
-				fs.writeFile(filepath, body, 'binary', function(err) {
+				//console.log("write download file " + filepath);
+				fs.writeFile(filepath[n], body, 'binary', function(err) {
 					if(err) {
 						return console.log(err);
 					}
-					//console.log("write download file " + filepath);
 					
-					//console.log('uploading to S3');
-					console.log(new Date())
-					fs.readFile(filepath, function(err, file_buffer){
-						var params = {
-							Bucket: bucket,
-							Key : 'ned_1_zip/' + filename,
-							Body: file_buffer
-						};
+					//unzip
+					fs.createReadStream(filepath[n]).pipe(unzip.Extract({ path: data_dir + '/dummy' }).on("close", function() {
+						console.log("unzipped");
 
-						s3.putObject(params, function (perr, pres) {
-							if (perr) {
-								console.log("Error uploading data: ", perr);
-							} else {
-								console.log("Successfully uploaded data");
-								console.log(new Date());
-									fs.unlink(filepath, function(err){
-										if(err) return console.log(err);
-										console.log('file deleted successfully');
-									});  
-							}
+					
+					
+						console.log('uploading to S3: ' + flt_filepath);
+						console.log(new Date())
+						fs.readFile(flt_filepath, function(err, file_buffer){
+							var params = {
+								Bucket: bucket,
+								Key : 'ned_1_zip/' + flt_filename,
+								Body: file_buffer
+							};
+
+							s3.putObject(params, function (perr, pres) {
+								if (perr) {
+									console.log("Error uploading data: ", perr);
+
+								} else {
+									console.log("Successfully uploaded data");
+									console.log(new Date());
+									
+									fs.appendFileSync(data_dir + '/file_list.txt', flt_filename + ' yes\n');
+									fs.emptyDirSync(data_dir + '/dummy');
+									if (n+1 < url.length) {
+										getFile(n+1);
+									}
+  
+								}
+							});
 						});
-					});
+					}));
 				});
 			
 			}
 			else {
-				//console.log('File not found: ' + url);
-			
+				console.log('File not found: ' + url[n]);
+				fs.appendFileSync(data_dir + '/file_list.txt', flt_filename + ' no\n');
+
+				if (n+1 < url.length) {
+					getFile(n+1);
+				}
 			}
 			
 		}
